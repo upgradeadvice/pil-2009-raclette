ab6c78fc1511ab3cdb844dadc7e989f5bf21319f
Fredrik Lundh <fredrik@effbot.org>
2009-03-07 05:32:44 +0100

    More crop tests.

--- Tests/test_image_crop.py
+++ Tests/test_image_crop.py
@@ -16,6 +16,13 @@ def test_crop():
     assert_equal(crop(0, 0, 100, 100), (0, 10000))
     assert_equal(crop(25, 25, 75, 75), (0, 2500))
 
+    # sides
+    assert_equal(crop(-25, 0, 25, 50), (1250, 1250))
+    assert_equal(crop(0, -25, 50, 25), (1250, 1250))
+    assert_equal(crop(75, 0, 125, 50), (1250, 1250))
+    assert_equal(crop(0, 75, 50, 125), (1250, 1250))
+
+    # corners
     assert_equal(crop(-25, -25, 25, 25), (1875, 625))
     assert_equal(crop(75, -25, 125, 25), (1875, 625))
     assert_equal(crop(75, 75, 125, 125), (1875, 625))
