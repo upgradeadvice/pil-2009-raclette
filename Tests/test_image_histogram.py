2bc258d8e34d0ceb4486fe8fa5ecd6407e3e728d
Fredrik Lundh <fredrik@effbot.org>
2009-03-07 14:56:01 +0100

    Fixed mode "F" histogram test.

--- Tests/test_image_histogram.py
+++ Tests/test_image_histogram.py
@@ -7,13 +7,17 @@ im = Image.open("Images/lena.ppm")
 def test_histogram():
 
     def histogram(mode):
-        h = im.convert(mode).histogram()
+        if mode == "F":
+            i = im.convert("L")
+        else:
+            i = im
+        h = i.convert(mode).histogram()
         return len(h), min(h), max(h)
 
     assert_equal(histogram("1"), (256, 0, 8872))
     assert_equal(histogram("L"), (256, 0, 199))
     assert_equal(histogram("I"), (256, 0, 199))
-    # assert_equal(histogram("F"), (256, 0, 199))
+    assert_equal(histogram("F"), (256, 0, 199))
     assert_equal(histogram("P"), (256, 0, 2912))
     assert_equal(histogram("RGB"), (768, 0, 285))
     assert_equal(histogram("RGBA"), (1024, 0, 16384))
