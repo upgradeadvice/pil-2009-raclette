4cf88ed2fecc7213d9b761d7f57a48032338995c
Fredrik Lundh <fredrik@effbot.org>
2009-03-07 13:38:57 +0100

    Added split test for modes I, F.
    --HG--
    branch : split32

--- Tests/test_image_split.py
+++ Tests/test_image_split.py
@@ -10,6 +10,8 @@ def test_split():
         return [(i.mode, i.size[0], i.size[1]) for i in layers]
     assert_equal(split("1"), [('1', 128, 128)])
     assert_equal(split("L"), [('L', 128, 128)])
+    assert_equal(split("I"), [('I', 128, 128)])
+    assert_equal(split("F"), [('F', 128, 128)])
     assert_equal(split("P"), [('P', 128, 128)])
     assert_equal(split("RGB"), [('L', 128, 128), ('L', 128, 128), ('L', 128, 128)])
     assert_equal(split("RGBA"), [('L', 128, 128), ('L', 128, 128), ('L', 128, 128), ('L', 128, 128)])
