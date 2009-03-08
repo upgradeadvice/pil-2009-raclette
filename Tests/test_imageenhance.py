44192ce6fc878109297a3d979d92e8965cae4453
Fredrik Lundh <fredrik@effbot.org>
2009-03-08 01:25:07 +0100

    More tests.

--- Tests/test_imageenhance.py
+++ Tests/test_imageenhance.py
@@ -3,4 +3,10 @@ from tester import *
 from PIL import Image
 from PIL import ImageEnhance
 
-success()
+def test_sanity():
+    
+    # FIXME: assert_image
+    assert_no_exception(lambda: ImageEnhance.Color(lena()).enhance(0.5))
+    assert_no_exception(lambda: ImageEnhance.Contrast(lena()).enhance(0.5))
+    assert_no_exception(lambda: ImageEnhance.Brightness(lena()).enhance(0.5))
+    assert_no_exception(lambda: ImageEnhance.Sharpness(lena()).enhance(0.5))
