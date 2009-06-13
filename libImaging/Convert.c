45bc3bd5266abaf5726397ca9433df1c107e1154
Alexey Borzenkov <snaury@gmail.com>
2009-06-13 16:11:58 +0200

    Fixed bug with RGB to BGR;15 conversion ---

--- libImaging/Convert.c
+++ libImaging/Convert.c
@@ -208,7 +208,7 @@ rgb2bgr15(UINT8* out_, const UINT8* in, int xsize)
     UINT16* out = (UINT16*) out_;
     for (x = 0; x < xsize; x++, in += 4)
 	*out++ =
-            ((((UINT16)in[0])<<8)&0x7c00) +
+            ((((UINT16)in[0])<<7)&0x7c00) +
             ((((UINT16)in[1])<<2)&0x03e0) +
             ((((UINT16)in[2])>>3)&0x001f);
 }
